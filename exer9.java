30557a3d719286b04722cc5d7c2e13faa3a6b7b8
Jefferson-Simplicio <107501814+Jefferson-Simplicio@users.noreply.github.com>
2022-07-13 09:38:52 -0300

    Add files via upload

---
+++ exer9.java
@@ -0,0 +1,16 @@
+package exe9;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class exer9 {
+    public static void main(String[] args) {
+        float n1, n2, media;
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite a Nota1: ");
+        n1 = ler.nextFloat();
+        System.out.print("Digite a Nota2: ");
+        n2 = ler.nextFloat();
+        media = (n1 + n2) / 2f;
+        JOptionPane.showMessageDialog(null, "Média: " + media);
+    }
+}
